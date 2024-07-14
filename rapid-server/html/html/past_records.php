7c5f8da6312b473ff9a40a6ac306ffa374145391
HongLiang1997 <60085590+HongLiang1997@users.noreply.github.com>
2024-07-14 17:48:03 +0800

    Dashboard - Update Script Fixed
    Fixed > Time and Date able to be updated
    Modified & Fixed > Blacklist and Whitelist can be updated

---
+++ rapid-server/html/html/past_records.php
@@ -0,0 +1,130 @@
+<?php
+session_start();
+?>
+<html lang="en">
+
+<head>
+    <?php
+    include "component/essential.inc.php";
+    ?>
+    <link rel="stylesheet" href="css/sessions.css">
+    <title>Past Records</title>
+</head>
+
+<body>
+    <main class="container-fluid">
+        <div class="row flex-nowrap">
+            <?php
+            include "component/sidebar.inc.php";
+            ?>
+            <div class="col py-3">
+                <div class="container content">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3>Past Records</h3><br>
+                            <a href="sessions.php"><button type="button"
+                                    class="btn btn-info btn-lg create">Sessions</button></a>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">History</h5>
+                                    <h6>Click on Status to start monitoring</h6>
+                                    <div class="d-flex justify-content-end">
+                                        <div clasclass="dropdown">
+                                            <button class="btn btn-secondary dropdown-toggle" type="button"
+                                                id="sortDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                                                Sort By
+                                            </button>
+                                            <ul class="dropdown-menu" aria-labelledby="sortDropdown">
+                                                <li><a class="dropdown-item" href="#" data-sort="session_id">Session
+                                                        ID</a></li>
+                                                <li><a class="dropdown-item" href="#" data-sort="name">Name</a></li>
+                                                <li><a class="dropdown-item" href="#" data-sort="status">Status</a></li>
+                                                <li><a class="dropdown-item" href="#" data-sort="date">Date</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="table-responsive">
+                                        <table class="table sessions">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Session ID</th>
+                                                    <th scope="col">Name</th>
+                                                    <th scope="col">Status</th>
+                                                    <th scope="col">Date</th>
+                                                    <th scope="col">Start Time</th>
+                                                    <th scope="col">End Time</th>
+                                                    <th scope="col">Duration(Mins)</th>
+                                                    <th scope="col">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="table-body">
+                                                <!-- Rows will be populated by JavaScript -->
+                                            </tbody>
+                                        </table>
+                                        <nav>
+                                            <ul class="pagination" id="pagination">
+                                                <!-- Pagination links will be populated by JavaScript -->
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- Edit Modal -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Edit Session</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="editForm">
+                        <div class="mb-3">
+                            <label for="editSessionName" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="editSessionName" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editSessionDate" class="form-label">Date</label>
+                            <input type="date" class="form-control" id="editSessionDate" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editSessionStartTime" class="form-label">Start Time</label>
+                            <input type="time" class="form-control" id="editSessionStartTime" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editSessionEndTime" class="form-label">End Time</label>
+                            <input type="time" class="form-control" id="editSessionEndTime" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editSessionDuration" class="form-label">Duration(Mins)</label>
+                            <input type="text" class="form-control" id="editSessionDuration" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="editBlacklist">Blacklist (comma-separated)</label>
+                            <input type="text" class="form-control" id="editBlacklist" name="blacklist" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="editWhitelist">Whitelist (comma-separated)</label>
+                            <input type="text" class="form-control" id="editWhitelist" name="whitelist" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<script defer src="js/index.js"></script>
+<script defer src="js/past_records.js"></script>
+
+</html>
