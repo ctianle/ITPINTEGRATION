285980cd7fe09902fc18d1c761abb3daa4bf5e50
ctianle <chuatianle@gmail.com>
2024-10-02 15:14:41 +0800

    update

--- LibComposite/autorndis1.sh
+++ LibComposite/autorndis1.sh
@@ -1,5 +1,5 @@
-echo "dtoverlay=dwc2 | sudo tee -a /boot/config.txt"
+echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
 sudo echo "libcomposite" | sudo tee -a /etc/modules
-sudo echo "dtoverlay=dwc2 >> /boot/firmware/config.txt
+sudo echo "dtoverlay=dwc2" >> /boot/firmware/config.txt
 sudo reboot
